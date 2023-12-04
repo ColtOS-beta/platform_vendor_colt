d087610115b60a2d45792df87401862cef0885fc
Ghosuto <clash.raja10@gmail.com>
2023-12-04 07:05:11 +0100

    vendor: Fix path location

--- config/common.mk
+++ config/common.mk
@@ -58,7 +58,7 @@ endif
 
 # Colt-specific init rc file
 PRODUCT_COPY_FILES += \
-    vendor/awaken/prebuilt/common/etc/init/init.awaken-system_ext.rc:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/init/init.awaken-system_ext.rc
+    vendor/colt/prebuilt/common/etc/init/init.colt-system_ext.rc:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/init/init.colt-system_ext.rc
 
 # Disable blur on app-launch
 PRODUCT_SYSTEM_EXT_PROPERTIES += \
