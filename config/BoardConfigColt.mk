61b07f49f3c57ddcbe26660b951e0ffba79cd5a7
Ghosuto <clash.raja10@gmail.com>
2024-01-04 12:05:16 +0530

    vendor: Include BoardConfigQcom in vendor

--- config/BoardConfigColt.mk
+++ config/BoardConfigColt.mk
@@ -1,7 +1,7 @@
 include vendor/colt/config/BoardConfigKernel.mk
 
 ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
-include hardware/qcom-caf/common/BoardConfigQcom.mk
+include vendor/colt/config/BoardConfigQcom.mk
 endif
 
 include vendor/colt/config/BoardConfigSoong.mk
